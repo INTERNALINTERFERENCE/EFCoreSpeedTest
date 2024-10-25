307db8d27c454e1d5b8a4c2cfb3735fde2c83b51
Anton Ryabukhin <ivefifthsence@gmail.com>
2024-10-25 19:25:28 +0300

    add migrations

---
+++ EFCoreSpeedTest.Storage/Migrations/20241025144841_AddTimeZone.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCoreSpeedTest.Storage.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTimeZone : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedAtTz",
+                table: "UserAccount",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAtTz",
+                table: "UserAccount");
+        }
+    }
+}
