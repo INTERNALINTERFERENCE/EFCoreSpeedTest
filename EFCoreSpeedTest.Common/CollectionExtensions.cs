d0e19eb2e16c57f2cbf979d124f16ef588fc7e45
Anton Ryabukhin <ivefifthsence@gmail.com>
2024-11-11 17:56:24 +0300

    fix collection extension

--- EFCoreSpeedTest.Common/CollectionExtensions.cs
+++ EFCoreSpeedTest.Common/CollectionExtensions.cs
@@ -6,7 +6,7 @@ public static class CollectionExtensions
 {
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static bool IsNullOrEmpty<T>(this IEnumerable<T>? collection) =>
-        collection is null || IsEmpty(collection);
+        collection is null || collection.IsEmpty();
     
     [MethodImpl( MethodImplOptions.AggressiveInlining )]
     public static bool IsEmpty<T>( this IEnumerable<T> @this ) =>
